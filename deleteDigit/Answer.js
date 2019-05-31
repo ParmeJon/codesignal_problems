1a48c3f7b601269e44c3b9248e991459fb6890d8
github username <github email address>
2019-05-31 00:32:14 -0400

    moree

---
+++ deleteDigit/Answer.js
@@ -0,0 +1,10 @@
+function deleteDigit(n) {
+let nString = n.toString()
+let highestNum = 0
+    for (let i = 0; i < nString.length; i++) {
+        if (parseInt(nString.slice(0, i) + nString.slice(i+1)) > highestNum) {
+            highestNum = parseInt(nString.slice(0, i) + nString.slice(i+1))
+        }
+    }
+    return highestNum
+}
