59afb33d0541c2caec75a0f8b2ff003140fda80a
github username <jonathansychan@gmail.com>
2019-06-07 14:16:19 -0400

    more

--- Circle of Numbers/Circle of Numbers.js
+++ Circle of Numbers/Circle of Numbers.js
@@ -7,3 +7,12 @@ let half = n/2
     }
     return firstNumber
 }
+
+function circleOfNumbers2(n, firstNumber) {
+let oppositeChange = n/2
+if (firstNumber === oppositeChange) {
+    return 0
+}
+return (firstNumber > oppositeChange) ? (firstNumber - oppositeChange) : (firstNumber + oppositeChange)
+
+}
