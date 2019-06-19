945696c80e92130ff1869bfbd32edea9684a40c6
github username <jonathansychan@gmail.com>
2019-06-19 14:37:30 -0400

    more

---
+++ isListPalindrome/Answer.js
@@ -0,0 +1,15 @@
+// Definition for singly-linked list:
+// function ListNode(x) {
+//   this.value = x;
+//   this.next = null;
+// }
+//
+function isListPalindrome(l) {
+    let arr = []
+    while (l !== null) {
+        arr.push(l.value)
+        l = l.next
+    }
+
+    return arr.join('') === arr.reverse().join('')
+}
